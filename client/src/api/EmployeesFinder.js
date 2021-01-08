73c47d0688cdbeb5e02404bb8355b3c8d7ca2db0
goran <goran.novacic@gmail.com>
2021-01-08 09:50:39 +0100

    #pre_aws

--- client/src/api/EmployeesFinder.js
+++ client/src/api/EmployeesFinder.js
@@ -1,5 +1,16 @@
 import axios from 'axios'
 
+//const baseURL = 'http://localhost:3006/api/v1/employees/';
+
+// NODE_ENV = development environment
+// NODE_ENV = production environment
+
+// if we are production we on /api/v1/employees
+
+const baseURL = process.env.NODE_ENV === 'production' 
+? '/api/v1/employees'
+: 'http://localhost/api/v1/employees';
+
 export default axios.create ({
-  baseUrl:'http://localhost:3006/api/v1/employees/'
+  baseURL.
 });
